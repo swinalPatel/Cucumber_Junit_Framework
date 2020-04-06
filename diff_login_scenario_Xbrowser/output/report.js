6d853e01b2b92dc4eb6c3fac66bc46ca71e40f27
swinal <swinal0110@gmail.com>
2020-04-06 10:52:54 +0100

    changes into runner class

--- diff_login_scenario_Xbrowser/output/report.js
+++ diff_login_scenario_Xbrowser/output/report.js
@@ -157,7 +157,7 @@ formatter.match({
   "location": "LoginSteps.I_go_to_(String,String)"
 });
 formatter.result({
-  "duration": 10734645000,
+  "duration": 8832543700,
   "status": "passed"
 });
 formatter.match({
@@ -174,7 +174,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 198501500,
+  "duration": 123983900,
   "status": "passed"
 });
 formatter.match({
@@ -191,7 +191,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 89184200,
+  "duration": 118433700,
   "status": "passed"
 });
 formatter.match({
@@ -204,7 +204,7 @@ formatter.match({
   "location": "LoginSteps.I_click_on(String)"
 });
 formatter.result({
-  "duration": 1351179700,
+  "duration": 1150207200,
   "status": "passed"
 });
 formatter.match({
@@ -217,7 +217,7 @@ formatter.match({
   "location": "LoginSteps.login_should_be(String)"
 });
 formatter.result({
-  "duration": 30274500,
+  "duration": 28645900,
   "status": "passed"
 });
 formatter.scenario({
@@ -279,7 +279,7 @@ formatter.match({
   "location": "LoginSteps.I_go_to_(String,String)"
 });
 formatter.result({
-  "duration": 1465730600,
+  "duration": 1069719300,
   "status": "passed"
 });
 formatter.match({
@@ -296,7 +296,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 250860900,
+  "duration": 60323500,
   "status": "passed"
 });
 formatter.match({
@@ -313,7 +313,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 149409400,
+  "duration": 131930000,
   "status": "passed"
 });
 formatter.match({
@@ -326,7 +326,7 @@ formatter.match({
   "location": "LoginSteps.I_click_on(String)"
 });
 formatter.result({
-  "duration": 765727800,
+  "duration": 302289400,
   "status": "passed"
 });
 formatter.match({
@@ -339,8 +339,8 @@ formatter.match({
   "location": "LoginSteps.login_should_be(String)"
 });
 formatter.result({
-  "duration": 145152400,
-  "error_message": "org.openqa.selenium.UnhandledAlertException: Dismissed user prompt dialog: User or Password is not valid: \nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-7C88RL8\u0027, ip: \u0027192.168.0.4\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.firefox.FirefoxDriver\nCapabilities {acceptInsecureCerts: true, browserName: firefox, browserVersion: 74.0, javascriptEnabled: true, moz:accessibilityChecks: false, moz:buildID: 20200309095159, moz:geckodriverVersion: 0.24.0, moz:headless: false, moz:processID: 26848, moz:profile: C:\\Users\\swina\\AppData\\Loca..., moz:shutdownTimeout: 60000, moz:useNonSpecCompliantPointerOrigin: false, moz:webdriverClick: true, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, platformVersion: 10.0, rotatable: false, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: e41413a3-e41f-4a45-943e-835a6a8a80c9\n*** Element info: {Using\u003dxpath, value\u003d//a[@href\u003d\u0027addcustomerpage.php\u0027]}\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:120)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementsByXPath(RemoteWebDriver.java:432)\r\n\tat org.openqa.selenium.By$ByXPath.findElements(By.java:348)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:311)\r\n\tat com.cucumber.MavenCucumberLogin.WebConnector.isElementPresent(WebConnector.java:79)\r\n\tat com.cucumber.MavenCucumberLogin.LoginSteps.login_should_be(LoginSteps.java:43)\r\n\tat ✽.Then login should be \"Unsuccess\"(login.feature:13)\r\n",
+  "duration": 84600800,
+  "error_message": "org.openqa.selenium.UnhandledAlertException: Dismissed user prompt dialog: User or Password is not valid: \nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-7C88RL8\u0027, ip: \u0027192.168.0.4\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.firefox.FirefoxDriver\nCapabilities {acceptInsecureCerts: true, browserName: firefox, browserVersion: 74.0, javascriptEnabled: true, moz:accessibilityChecks: false, moz:buildID: 20200309095159, moz:geckodriverVersion: 0.24.0, moz:headless: false, moz:processID: 19240, moz:profile: C:\\Users\\swina\\AppData\\Loca..., moz:shutdownTimeout: 60000, moz:useNonSpecCompliantPointerOrigin: false, moz:webdriverClick: true, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, platformVersion: 10.0, rotatable: false, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 62930f09-297c-4cd5-929c-d1ac4dfedd6c\n*** Element info: {Using\u003dxpath, value\u003d//a[@href\u003d\u0027addcustomerpage.php\u0027]}\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:120)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementsByXPath(RemoteWebDriver.java:432)\r\n\tat org.openqa.selenium.By$ByXPath.findElements(By.java:348)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:311)\r\n\tat com.cucumber.MavenCucumberLogin.WebConnector.isElementPresent(WebConnector.java:79)\r\n\tat com.cucumber.MavenCucumberLogin.LoginSteps.login_should_be(LoginSteps.java:43)\r\n\tat ✽.Then login should be \"Unsuccess\"(login.feature:13)\r\n",
   "status": "failed"
 });
 formatter.scenario({
@@ -402,7 +402,7 @@ formatter.match({
   "location": "LoginSteps.I_go_to_(String,String)"
 });
 formatter.result({
-  "duration": 11967353200,
+  "duration": 11857203900,
   "status": "passed"
 });
 formatter.match({
@@ -419,7 +419,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 1105759400,
+  "duration": 317077600,
   "status": "passed"
 });
 formatter.match({
@@ -436,7 +436,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 205723300,
+  "duration": 423261600,
   "status": "passed"
 });
 formatter.match({
@@ -449,7 +449,7 @@ formatter.match({
   "location": "LoginSteps.I_click_on(String)"
 });
 formatter.result({
-  "duration": 653281900,
+  "duration": 733447200,
   "status": "passed"
 });
 formatter.match({
@@ -462,8 +462,8 @@ formatter.match({
   "location": "LoginSteps.login_should_be(String)"
 });
 formatter.result({
-  "duration": 13908200,
-  "error_message": "org.openqa.selenium.UnhandledAlertException: unexpected alert open: {Alert text : User or Password is not valid}\n  (Session info: chrome\u003d80.0.3987.149): User or Password is not valid\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-7C88RL8\u0027, ip: \u0027192.168.0.4\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\swina\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:52892}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 3bb207545f827b24fc0bda0bbbd530c1\n*** Element info: {Using\u003dxpath, value\u003d//a[@href\u003d\u0027addcustomerpage.php\u0027]}\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:120)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementsByXPath(RemoteWebDriver.java:432)\r\n\tat org.openqa.selenium.By$ByXPath.findElements(By.java:348)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:311)\r\n\tat com.cucumber.MavenCucumberLogin.WebConnector.isElementPresent(WebConnector.java:79)\r\n\tat com.cucumber.MavenCucumberLogin.LoginSteps.login_should_be(LoginSteps.java:43)\r\n\tat ✽.Then login should be \"Unsuccess\"(login.feature:13)\r\n",
+  "duration": 27658300,
+  "error_message": "org.openqa.selenium.UnhandledAlertException: unexpected alert open: {Alert text : User or Password is not valid}\n  (Session info: chrome\u003d80.0.3987.149): User or Password is not valid\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027DESKTOP-7C88RL8\u0027, ip: \u0027192.168.0.4\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_241\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\swina\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:51619}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: 320fa4e9008a30763883239f710852ef\n*** Element info: {Using\u003dxpath, value\u003d//a[@href\u003d\u0027addcustomerpage.php\u0027]}\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:120)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementsByXPath(RemoteWebDriver.java:432)\r\n\tat org.openqa.selenium.By$ByXPath.findElements(By.java:348)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElements(RemoteWebDriver.java:311)\r\n\tat com.cucumber.MavenCucumberLogin.WebConnector.isElementPresent(WebConnector.java:79)\r\n\tat com.cucumber.MavenCucumberLogin.LoginSteps.login_should_be(LoginSteps.java:43)\r\n\tat ✽.Then login should be \"Unsuccess\"(login.feature:13)\r\n",
   "status": "failed"
 });
 formatter.scenario({
@@ -525,7 +525,7 @@ formatter.match({
   "location": "LoginSteps.I_go_to_(String,String)"
 });
 formatter.result({
-  "duration": 1234568900,
+  "duration": 1487044000,
   "status": "passed"
 });
 formatter.match({
@@ -542,7 +542,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 281339500,
+  "duration": 287943400,
   "status": "passed"
 });
 formatter.match({
@@ -559,7 +559,7 @@ formatter.match({
   "location": "LoginSteps.I_enter_username_password(String,String)"
 });
 formatter.result({
-  "duration": 459623900,
+  "duration": 241525200,
   "status": "passed"
 });
 formatter.match({
@@ -572,7 +572,7 @@ formatter.match({
   "location": "LoginSteps.I_click_on(String)"
 });
 formatter.result({
-  "duration": 1092194500,
+  "duration": 866897600,
   "status": "passed"
 });
 formatter.match({
@@ -585,7 +585,7 @@ formatter.match({
   "location": "LoginSteps.login_should_be(String)"
 });
 formatter.result({
-  "duration": 66513000,
+  "duration": 60792400,
   "status": "passed"
 });
 });
